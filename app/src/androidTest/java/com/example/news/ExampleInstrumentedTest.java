c21a2a2959fef9c663987eb9f8fcaf156280c6f3
pratheeksha <pratheeksha.j@ltts.com>
2020-11-07 17:15:52 +0530

    Some Errors are still there

--- app/src/androidTest/java/com/example/news/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/news/ExampleInstrumentedTest.java
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.news", appContext.getPackageName());
+
+        assertEquals("com.example.covid19", appContext.getPackageName());
     }
 }
